fa5d35484b23247b8f4335d273a7dad229747e9d
foxrw1 <78601276+foxrw1@users.noreply.github.com>
2022-02-04 08:06:17 -0500

    Add files via upload

---
+++ Spring Break.cpp
@@ -0,0 +1,19 @@
+// Spring Break
+#include <iostream>
+#include <fstream>
+using namespace std;
+
+int main()
+{
+    //Create an input file variable
+    ifstream fin;
+    //Open the file
+    fin.open("vacation.txt"); //make sure you create this file!
+
+  
+
+
+
+    system("pause");
+}
+
